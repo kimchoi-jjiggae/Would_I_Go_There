47b312a463bffb140d979640b650b72c1bcb55b8
Michelle <choi.michelle.12@gmail.com>
2023-02-10 17:05:26 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// let map;
+
+// function initMap( lat= -34.397, lng= 150.644) {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//   });
+// }
+
+// window.initMap = initMap;
